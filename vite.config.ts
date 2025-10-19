56eef055f30043a76a2767d7a1e24ad694ec3b67
alrzakh <kh.ramin69@gmail.com>
2025-10-19 21:49:23 +0330

    Update version in package.json to 0.3.0 and modify base URL in vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
 		}),
 		tailwindcss()
 	],
-	base: '/alrzakh.github.io/' // No repo name in URL for username.github.io repo
+	base: '/' // No repo name in URL for username.github.io repo
 })
